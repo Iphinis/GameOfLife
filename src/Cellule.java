a045ed035bd0261d91f8cd569a5062d902f62da6
DADABJSD <benjellounsaad2002@gmail.com>
2024-03-21 23:52:14 +0100

    Règles vie cellules
    MAJ règles de naissance/mort d'une cellule

--- src/Cellule.java
+++ src/Cellule.java
@@ -2,31 +2,33 @@ import java.util.List;
 import java.util.ArrayList;
 
 class Cellule {
-	public boolean enVie = false;
+    public boolean enVie = false;
 
-	public List<Cellule> voisins = new ArrayList<Cellule>();
+    public List<Cellule> voisins = new ArrayList<Cellule>();
 
-	private int voisinsVivants() {
-		int s = 0;
-		int n = voisins.size();
-		for(int i=0; i < n; i++) {
-			if(voisins.get(i).enVie) s += 1;
-		}
-		return s;
-	}
+    private int voisinsVivants() {
+        int s = 0;
+        int n = voisins.size();
+        for (int i = 0; i < n; i++) {
+            if (voisins.get(i).enVie) s += 1;
+        }
+        return s;
+    }
 
-	public void evoluer() {
-		int v = voisinsVivants();
+    public void evoluer() {
+        int v = voisinsVivants();
 
-		switch(v) {
-			case 3:
-				enVie = true;
-				break;
-			case 2:
-				break;
-			default:
-				enVie = false;
-				break;
-		}
-	}
+        if (enVie) {
+            // Règles pour une cellule vivante
+            if (v < 2 || v > 3) {
+                enVie = false; // Mort par sous-population ou surpopulation
+            }
+        } else {
+            // Règles pour une cellule morte
+            if (v == 3) {
+                enVie = true; // Naissance
+            }
+        }
+    }
 }
+
